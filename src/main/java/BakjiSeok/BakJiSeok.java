7bcd7f53cb19270cdd03619ca8f9ee6cb29db3d2
BakJiSeok <commicat2@gmail.com>
2025-07-21 20:38:53 +0900

    feat: add BakJiSeok.java

---
+++ src/main/java/BakjiSeok/BakJiSeok.java
@@ -0,0 +1,43 @@
+package BakjiSeok;
+
+public class BakJiSeok {
+  private String a;
+  private int b;
+
+  public BakJiSeok(String a, int b) {
+    this.a = a;
+    this.b = b;
+  }
+
+  public BakJiSeok(String a) {
+    this(a, 0);
+  }
+
+  public BakJiSeok() {
+    this(null, 0);
+  }
+
+  public String getA() {
+    return a;
+  }
+
+  public void setA(String a) {
+    this.a = a;
+  }
+
+  public int getB() {
+    return b;
+  }
+
+  public void setB(int b) {
+    this.b = b;
+  }
+
+  @Override
+  public String toString() {
+    return "BakJiSeok{" +
+        "a='" + a + '\'' +
+        ", b=" + b +
+        '}';
+  }
+}
