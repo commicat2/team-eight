426103f47e99aa65e64be7283532e53c955c66bb
BakJiSeok <commicat2@gmail.com>
2025-07-21 20:57:47 +0900

    feat: rename a, b to name, age

--- src/main/java/BakJiSeok/BakJiSeok.java
+++ src/main/java/BakJiSeok/BakJiSeok.java
@@ -1,43 +1,43 @@
 package BakJiSeok;
 
 public class BakJiSeok {
-  private String a;
-  private int b;
+  private String name;
+  private int age;
 
-  public BakJiSeok(String a, int b) {
-    this.a = a;
-    this.b = b;
+  public BakJiSeok(String name, int age) {
+    this.name = name;
+    this.age = age;
   }
 
-  public BakJiSeok(String a) {
-    this(a, 0);
+  public BakJiSeok(String name) {
+    this(name, 0);
   }
 
   public BakJiSeok() {
     this("", 0);
   }
 
-  public String getA() {
-    return a;
+  public String getName() {
+    return name;
   }
 
-  public void setA(String a) {
-    this.a = a;
+  public void setName(String name) {
+    this.name = name;
   }
 
-  public int getB() {
-    return b;
+  public int getAge() {
+    return age;
   }
 
-  public void setB(int b) {
-    this.b = b;
+  public void setAge(int age) {
+    this.age = age;
   }
 
   @Override
   public String toString() {
     return "BakJiSeok{" +
-        "a='" + a + '\'' +
-        ", b=" + b +
+        "name='" + name + '\'' +
+        ", age=" + age +
         '}';
   }
 }
