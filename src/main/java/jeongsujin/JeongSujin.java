05eebae5cc611c28f8c5f684ed1fb83d785c66e0
Jeong Sujin <wjdtnwls0106@gmail.com>
2025-07-23 17:27:00 +0900

    내 브랜치에서 내 파일 또 수정

--- src/main/java/jeongsujin/JeongSujin.java
+++ src/main/java/jeongsujin/JeongSujin.java
@@ -5,14 +5,16 @@ import java.util.Objects;
 public class JeongSujin {
     private String name;
     private int age;
+    private int number;
 
 
     private JeongSujin() {
     }
 
-    public JeongSujin(String name, int age) {
+    public JeongSujin(String name, int age, int number) {
         this.name = name;
         this.age = age;
+        this.number = number;
     }
 
     public String getName() {
@@ -23,11 +25,16 @@ public class JeongSujin {
         return age;
     }
 
+    public int getNumber() {
+        return number;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("JeongSujin{");
         sb.append("name='").append(name).append('\'');
         sb.append(", age=").append(age);
+        sb.append(", number=").append(number);
         sb.append('}');
         return sb.toString();
     }
@@ -36,11 +43,11 @@ public class JeongSujin {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         JeongSujin that = (JeongSujin) o;
-        return age == that.age && Objects.equals(name, that.name);
+        return age == that.age && number == that.number && Objects.equals(name, that.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, age);
+        return Objects.hash(name, age, number);
     }
 }
